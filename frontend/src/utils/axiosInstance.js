d86c5f85c8619e244c7c41255a5aa13ff75dc1d4
SourabhPatidar711 <sourabhnote9@gmail.com>
2025-11-07 17:15:15 +0530

    remove latency

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL:"http://localhost:3000",
-    timeout: 10000,
+    baseURL:"https://url-shorter-tawny.vercel.app/",
     withCredentials: true
 })
 
