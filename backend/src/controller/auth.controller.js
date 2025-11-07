974cbbd5bdd34a04dc02a30f26a56b35712b351e
SourabhPatidar711 <sourabhnote9@gmail.com>
2025-11-07 19:03:37 +0530

    jwt

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -11,16 +11,22 @@ export const register_user = wrapAsync( async (req, res) => {
         httpOnly: true,     
         secure: true,     
         sameSite: "None",
+        maxAge: 7 * 24 * 60 * 60 * 1000,
     });
-    res.status(200).json({message:"register success"})
+    res.status(200).json({message:"register success",token})
 })
 
 export const login_user = wrapAsync( async (req, res) => {
     const {email, password} = req.body
     const {token,user} = await loginUser(email, password)
     req.user = user
-    res.cookie("accessToken", token, cookieOptions)
-    res.status(200).json({user:user,message:"login success"})
+    res.cookie("accessToken", token, {
+        httpOnly: true,     
+        secure: true,     
+        sameSite: "None",
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+    res.status(200).json({user:user,message:"login success",token})
 })
 
 export const logout_user = wrapAsync( async (req, res) => {
--- backend/src/utils/helper.js
+++ backend/src/utils/helper.js
@@ -1,17 +1,20 @@
 import { nanoid } from "nanoid";
-import { cookieOptions } from "../config/config.js";
-import jsonwebtoken from "jsonwebtoken"
+import jwt from "jsonwebtoken"
 
 export const generateNanoId = (length) =>{
     return nanoid(length);
 }
 
 export const signToken = (payload) =>{
-    return jsonwebtoken.sign(payload, process.env.JWT_SECRET, {expiresIn: "1h"})
+    return jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: "1h"})
 }
 
-export const verifyToken = (token) =>{
-
-    const decoded = jsonwebtoken.verify(token, process.env.JWT_SECRET)
-    return decoded.id
-}
+export const verifyToken = (token) => {
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    return decoded?.id;
+  } catch (error) {
+    console.error("❌ Token verification failed:", error.message);
+    return null;
+  }
+};
